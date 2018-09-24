39c8e7061cbaf72b838425084ad67b566598c5ec
Steven Dols <stevendols5798@pointloma.edu>
2018-09-23 19:24:17 -0700

    Create PlayerController Script
    Allow Player to Move Forward/Backward
    based on user input to keyboard.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 5f;
+
+    private Rigidbody rb;
+    private float moveInput;
+    private float turnInput;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //store the rigidbody component
+        rb = GetComponent<Rigidbody>();
+	}
+
+    private void Update()
+    {
+        moveInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate ()
+    {
+        MovePlayer();
+
+	}
+
+    private void MovePlayer()
+    {
+        Vector3 movement = transform.forward * moveInput * playerSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
+    }
+}
