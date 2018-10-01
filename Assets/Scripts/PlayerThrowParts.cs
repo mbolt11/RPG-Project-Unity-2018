a2e5c105d6f10ab3f64228a6a2495b69c1e043f7
Maria Bolt <mariabolt@Marias-MacBook-Pro.local>
2018-10-01 16:07:01 -0700

    Made player's arm move when he throws

--- Assets/Scripts/PlayerThrowParts.cs
+++ Assets/Scripts/PlayerThrowParts.cs
@@ -8,18 +8,27 @@ public class PlayerThrowParts : MonoBehaviour {
     public Transform weaponSpawn;
     public float velocity;
 
+    public GameObject playerArm;
+    private Transform armtransform;
+
     private string fireButton;
     private bool fired;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         fireButton = "Fire1";
-	}
+
+        //Get the transform of the player's arm for later
+        armtransform = playerArm.GetComponent<Transform>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
         if (Input.GetButtonUp(fireButton) && !fired)
         {
+            //Fire the wrench
             fired = true;
             Fire();
         }
@@ -27,8 +36,22 @@ public class PlayerThrowParts : MonoBehaviour {
 
     private void Fire()
     {
+        //Save the starting arm position
+        Vector3 currentarmpos = armtransform.position;
+        Quaternion currentarmrot = armtransform.rotation;
+
+        //Move the player's arm to show him throwing
+        Vector3 armposition1 = new Vector3(armtransform.position.x, armtransform.position.y, armtransform.position.z + 0.5f);
+        Quaternion armrotation1 = Quaternion.Euler(armtransform.rotation.x + 90, armtransform.rotation.y, armtransform.rotation.z);
+        armtransform.SetPositionAndRotation(armposition1, armrotation1);
+
+        //Throw the wrench
         Rigidbody wrenchInstance = Instantiate(wrench, weaponSpawn.position, weaponSpawn.rotation) as Rigidbody;
         wrenchInstance.velocity = velocity * weaponSpawn.forward;
         fired = false;
+
+        /* Need to find a way to pause slightly before returning the arm to position?
+        //Move the player's arm back to starting position
+        armtransform.SetPositionAndRotation(currentarmpos, currentarmrot);*/
     }
 }
