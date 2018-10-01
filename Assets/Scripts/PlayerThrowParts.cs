8bba77b4b975a441c4dc366c311936086ec9d79a
ATIMMONS <atimmons277@pointloma.edu>
2018-09-30 22:24:11 -0700

    Added player throwing wrench w/space key and started particle system to instantiate when wrench hits the robot

---
+++ Assets/Scripts/PlayerThrowParts.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerThrowParts : MonoBehaviour {
+
+    public Rigidbody wrench;
+    public Transform weaponSpawn;
+    public float velocity;
+
+    private string fireButton;
+    private bool fired;
+
+	// Use this for initialization
+	void Start () {
+        fireButton = "Fire1";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonUp(fireButton) && !fired)
+        {
+            fired = true;
+            Fire();
+        }
+	}
+
+    private void Fire()
+    {
+        Rigidbody wrenchInstance = Instantiate(wrench, weaponSpawn.position, weaponSpawn.rotation) as Rigidbody;
+        wrenchInstance.velocity = velocity * weaponSpawn.forward;
+        fired = false;
+    }
+}
---
+++ Assets/Scripts/WrenchObliteration.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrenchObliteration : MonoBehaviour {
+
+    public float MaxLifeTime = 4f;
+    public ParticleSystem ObliterationParticles;
+    
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, MaxLifeTime);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if the robot is hit then special noise, particles, and event occurs??
+        if(other.CompareTag("Robot"))
+        {
+            //damage to the robot
+        }
+
+        //ObliterationParticles.transform.parent = null;
+        ObliterationParticles.Play();
+
+        Destroy(ObliterationParticles, ObliterationParticles.main.duration);
+        Destroy(gameObject);
+    }
+}
