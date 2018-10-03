fe53f91f57a4b398a304607282383f2935417e2d
Maria Bolt <mariabolt@Marias-MacBook-Pro.local>
2018-10-03 16:18:19 -0700

    Added mountains, adjusted cameras, working on player arm

--- Assets/Scripts/PlayerThrowParts.cs
+++ Assets/Scripts/PlayerThrowParts.cs
@@ -9,25 +9,24 @@ public class PlayerThrowParts : MonoBehaviour {
     public float velocity;
 
     public GameObject playerArm;
+    public GameObject playerBody;
     private Transform armtransform;
+    private Transform bodytransform;
 
     private string fireButton;
     private bool fired;
 
-    private Vector3 normalarmpos;
-    private Quaternion normalarmrot;
+    private Vector3 currentarmpos;
+    private Quaternion currentarmrot;
 
 	// Use this for initialization
 	void Start ()
     {
         fireButton = "Fire1";
 
-        //Get the transform of the player's arm for later
+        //Get the transform of the player's arm and body for later
         armtransform = playerArm.GetComponent<Transform>();
-
-        //Save the starting arm position
-        normalarmpos = armtransform.position;
-        normalarmrot = armtransform.rotation;
+        bodytransform = playerBody.GetComponent<Transform>();
     }
 	
 	// Update is called once per frame
@@ -43,23 +42,40 @@ public class PlayerThrowParts : MonoBehaviour {
 
     private void Fire()
     {
-        //Move the player's arm to show him throwing
+        //Save the starting arm position
+        //currentarmpos = armtransform.position;
+        //currentarmrot = armtransform.rotation;
+
+        /*Move the player's arm to show him throwing
         Vector3 armposition1 = new Vector3(armtransform.position.x, armtransform.position.y, armtransform.position.z + 0.5f);
         Quaternion armrotation1 = Quaternion.Euler(armtransform.rotation.x + 90, armtransform.rotation.y, armtransform.rotation.z);
+        armtransform.SetPositionAndRotation(armposition1, armrotation1);*/
+
+        //Move the player's arm to show him throwing
+        Vector3 armposition1 = new Vector3(bodytransform.position.x + 0.7f, bodytransform.position.y + 0.5f, bodytransform.position.z);
+        Quaternion armrotation1 = Quaternion.Euler(bodytransform.localRotation.x + 270, bodytransform.localRotation.y, bodytransform.localRotation.z - 150);
         armtransform.SetPositionAndRotation(armposition1, armrotation1);
 
         //Throw the wrench
         Rigidbody wrenchInstance = Instantiate(wrench, weaponSpawn.position, weaponSpawn.rotation) as Rigidbody;
         wrenchInstance.velocity = velocity * weaponSpawn.forward;
-        StartCoroutine("ResetArm");
         fired = false;
+
+        //ResetArm the arm position after throwing
+        StartCoroutine("ResetArm");
     }
 
     private IEnumerator ResetArm()
     {
-        yield return new WaitForSeconds(.1f);
+        yield return new WaitForSeconds(0.3f);
 
-        //Move the player's arm back to starting position
-        armtransform.SetPositionAndRotation(normalarmpos, normalarmrot);
+        /*Move the player's arm back to poised position
+        Vector3 armposition2 = new Vector3(armtransform.position.x, armtransform.position.y, armtransform.position.z - 0.5f);
+        Quaternion armrotation2 = Quaternion.Euler(armtransform.rotation.x - 90, armtransform.rotation.y, armtransform.rotation.z);
+        armtransform.SetPositionAndRotation(armposition2, armrotation2);*/
+
+        Vector3 armposition2 = new Vector3(bodytransform.position.x + 0.7f, bodytransform.position.y + 0.5f, bodytransform.position.z);
+        Quaternion armrotation2 = Quaternion.Euler(bodytransform.localRotation.x + 180, bodytransform.localRotation.y, bodytransform.localRotation.z - 150);
+        armtransform.SetPositionAndRotation(armposition2, armrotation2);
     }
 }
