357b1ea091c70023afdbde7715b270db72920a98
ATIMMONS <atimmons277@pointloma.edu>
2018-10-11 07:37:56 -0700

    Robot now throws weapons

---
+++ Assets/RobotWeaponObliteration.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotWeaponObliteration : MonoBehaviour
+{
+
+    public float MaxLifeTime = 4f;
+    public ParticleSystem ObliterationParticles;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, MaxLifeTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if the robot is hit then special noise, particles, and event occurs??
+        if (other.CompareTag("Player"))
+        {
+            //Effect when hitting the robot
+            ObliterationParticles.Play();
+        }
+        Debug.Log(other.tag);
+        ObliterationParticles.transform.parent = null;
+
+        Destroy(ObliterationParticles.gameObject, ObliterationParticles.main.duration);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/RobotThrowsParts.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotThrowsParts : MonoBehaviour
+{
+
+    public Rigidbody cog;
+    public Transform weaponSpawn;
+    public float velocity;
+    public int secondsBetweenThrow;
+
+    private int framestoThrow;
+    private int frames;
+
+    // Use this for initialization
+    void Start()
+    {
+        framestoThrow = 60 * secondsBetweenThrow;
+        frames = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frames++;
+
+        if (frames >= framestoThrow)
+        {
+            //Fire the parts
+            Fire();
+            frames = 0;
+        }
+    }
+
+    private void Fire()
+    {
+        Rigidbody cogInstance = Instantiate(cog, weaponSpawn.position, weaponSpawn.rotation) as Rigidbody;
+        cogInstance.velocity = velocity * weaponSpawn.forward;
+    }
+}
