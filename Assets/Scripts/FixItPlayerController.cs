d065ba2b48ee534702710ec7cb30a9b5bcb44b17
Steven Dols <stevendols5798@pointloma.edu>
2018-10-15 20:48:46 -0700

    Consolidate/Fix Player Scripts

---
+++ Assets/Scripts/FixItPlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixItPlayerController : MonoBehaviour
+{
+    public GameObject playerBody;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "CommonPart")
+        {
+            Debug.Log("part hit");
+            playerBody.transform.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else if (other.tag == "OutsidePart")
+        {
+            Debug.Log("part hit");
+            playerBody.transform.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+
+}
--- Assets/Scripts/FixItPlayerMovement.cs
+++ Assets/Scripts/FixItPlayerMovement.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class FixItPlayerMovement : MonoBehaviour
 {
     public GameObject player;
+    public GameObject robotTarget;
     private Rigidbody rb;
-
-    private CollisionWithRobot collisionscript;
-    private char lastkeypressed;
+    private Transform rtTransform;
 
     private void Start()
     {
@@ -21,7 +20,9 @@ public class FixItPlayerMovement : MonoBehaviour
 
         //Get the player rigidbody and the player collision script
         rb = player.GetComponent<Rigidbody>();
-        collisionscript = player.GetComponent<CollisionWithRobot>();
+
+        //Get the robot target transform
+        rtTransform = robotTarget.GetComponent<Transform>();
     }
 
     // Move the player target by increments of 2 on button down
@@ -29,92 +30,99 @@ public class FixItPlayerMovement : MonoBehaviour
     {
         if (Input.GetKeyDown("w"))
         {
-            //Move the player target only if it won't go past the edge
+            //Hold the value for where the player will be once moved
+            Vector3 futureplayerpos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 2);
+
+            //Get the current robot target transform position
+            Vector3 robotposition = new Vector3(rtTransform.position.x, rtTransform.position.y, rtTransform.position.z);
+
+            //Check if movement will put player past the edge
             if (transform.position.z < 8)
             {
-                var pos = transform.position;
-                pos.z += 2;
-                transform.position = pos;
+                //Check if movement will put the player where the robot is
+                if (futureplayerpos != robotposition)
+                {
+                    //Move the player foward 2 units
+                    var pos = transform.position;
+                    pos.z += 2;
+                    transform.position = pos;
+                }
             }
             //Rotate the way the player is facing toward front
             rb.rotation = Quaternion.Euler(0f, 0f, 0f);
-            lastkeypressed = 'w';
         }
 
         if (Input.GetKeyDown("a"))
         {
-            //Move the player target only if it won't go past the edge
+            //Hold the value for where the player will be once moved
+            Vector3 futureplayerpos = new Vector3(transform.position.x - 2, transform.position.y, transform.position.z);
+
+            //Get the current robot target transform position
+            Vector3 robotposition = new Vector3(rtTransform.position.x, rtTransform.position.y, rtTransform.position.z);
+
+            //Check if the movement will put the player past the edge
             if (transform.position.x > -8)
             {
-                var pos = transform.position;
-                pos.x -= 2;
-                transform.position = pos;
+                //Check if the movement will put the player where the robot is
+                if (futureplayerpos != robotposition)
+                {
+                    //Move the player left 2 units
+                    var pos = transform.position;
+                    pos.x -= 2;
+                    transform.position = pos;
+                }
             }
             //Rotate the way the player is facing toward the left
             rb.rotation = Quaternion.Euler(0f, -90f, 0f);
-            lastkeypressed = 'a';
         }
 
         if (Input.GetKeyDown("s"))
         {
-            //Move the player target only if it won't go past the edge
+            //Hold the value for where the player will be once moved
+            Vector3 futureplayerpos = new Vector3(transform.position.x, transform.position.y, transform.position.z - 2);
+
+            //Get the current robot target transform position
+            Vector3 robotposition = new Vector3(rtTransform.position.x, rtTransform.position.y, rtTransform.position.z);
+
+            //Check if the movement will put the player past the edge
             if (transform.position.z > -6)
             {
-                var pos = transform.position;
-                pos.z -= 2;
-                transform.position = pos;
+                //Check if the movement will put the player where the robot is
+                if (futureplayerpos != robotposition)
+                {
+                    //Move the player backward 2 units
+                    var pos = transform.position;
+                    pos.z -= 2;
+                    transform.position = pos;
+                }
             }
             //Rotate the way the player is facing toward back
             rb.rotation = Quaternion.Euler(0f, 180f, 0f);
-            lastkeypressed = 's';
         }
 
         if (Input.GetKeyDown("d"))
         {
-            //Move the player target only if it won't go past the edge
+            //Hold the value for where the player will be once moved
+            Vector3 futureplayerpos = new Vector3(transform.position.x + 2, transform.position.y, transform.position.z);
+
+            //Get the current robot target transform position
+            Vector3 robotposition = new Vector3(rtTransform.position.x, rtTransform.position.y, rtTransform.position.z);
+
+            //Check if the movement will put the player past the edge
             if (transform.position.x < 6)
             {
-                var pos = transform.position;
-                pos.x += 2;
-                transform.position = pos;
+                //Check if the movement will put the player where the robot is
+                if (futureplayerpos != robotposition)
+                {
+                    //Move the player right 2 units
+                    var pos = transform.position;
+                    pos.x += 2;
+                    transform.position = pos;
+                }
             }
             //Rotate the way the player is facing toward the right
             rb.rotation = Quaternion.Euler(0f, 90f, 0f);
-            lastkeypressed = 'd';
-        }
-    }
-
-    private void LateUpdate()
-    {
-        //If the player collided with the robot, move it back
-        if (collisionscript.hasCollided)
-        {
-            //The direction you move it back depends on what key was pressed to cause the collision
-            switch(lastkeypressed)
-            {
-                case 'w':
-                    var posW = transform.position;
-                    posW.z -= 2;
-                    transform.position = posW;
-                    break;
-                case 'a':
-                    var posA = transform.position;
-                    posA.x += 2;
-                    transform.position = posA;
-                    break;
-                case 's':
-                    var posS = transform.position;
-                    posS.z += 2;
-                    transform.position = posS;
-                    break;
-                case 'd':
-                    var posD = transform.position;
-                    posD.x -= 2;
-                    transform.position = posD;
-                    break;
-            }
-            //After moving the player back, reset hasCollided
-            collisionscript.hasCollided = false;
         }
     }
 }
+
