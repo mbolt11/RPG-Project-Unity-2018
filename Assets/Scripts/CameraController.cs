4fb4fdf4d78c0550def305070919aba807eadcff
Steven Dols <stevendols5798@pointloma.edu>
2018-10-22 11:13:55 -0700

    Struggling With Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    public GameObject player;
+    public Transform player;
 
-    private Vector3 offset;
+    //private Vector3 offset;
 
     //Set the offset of the camera based on the start position of camera and player
     void Start()
     {
-        offset = transform.position - player.transform.position;
+
     }
 
     //Update the camera position based on where the player moves
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = new Vector3(0f, player.position.y + 2, player.position.z - 4);
+        transform.rotation = Quaternion.identity;
+        print(ToString());
+        print(transform.position);
+
     }
 }
