625b00366e8a3320814af0e24396e3cdb1346f34
Steven Dols <stevendols5798@pointloma.edu>
2018-10-24 14:46:02 -0700

    Changed Player Movement and Follow Cam

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,24 +4,14 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
-    public Transform player;
-
-    //private Vector3 offset;
-
-    //Set the offset of the camera based on the start position of camera and player
     void Start()
     {
 
     }
 
-    //Update the camera position based on where the player moves
+
     void LateUpdate()
     {
-        transform.position = new Vector3(0f, player.position.y + 2, player.position.z - 4);
-        transform.rotation = Quaternion.identity;
-        //print(ToString());
-        //print(transform.position);
-
+        
     }
 }
